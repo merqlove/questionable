fb5e4159dbcb1b1c93094375fd63ac1d2f315fd5
Alexander Merkulov <sasha@merqlove.ru>
2013-02-01 21:21:00 +0400

    enable Answer select by user

--- app/models/questionable/answer.rb
+++ app/models/questionable/answer.rb
@@ -5,9 +5,11 @@ module Questionable
     belongs_to :user
     belongs_to :assignment
     belongs_to :option
+ #   belongs_to :comment
     has_one :question, :through => :assignment
+   # has_one :question_group, :through => :assignment
 
-=begin
+#=begin
     def self.build_answers_for_subject(user, subject)
       if subject.kind_of?(Symbol) or subject.kind_of?(String)
         assignments = Questionable::Assignment.where(:subject_type => subject)
@@ -18,7 +20,7 @@ module Questionable
       assignments = assignments.order(:position)
       assignments.map { |as| as.answers_for_user(user) }
     end
-=end
+#=end
 
     #  Questionable::Question.joins('INNER JOIN questionable_assignments ON questionable_assignments.question_id = questionable_questions.id').where(:questionable_assignments => { :subject_type => type }).order('questionable_assignments.position')
 
